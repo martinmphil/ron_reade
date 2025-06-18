bf1f0fceadf1e19ecdb6ef89bc3d99e7cecbfbd9
Martin <martin.mphil@gmail.com>
2025-06-18 10:58:08 +0100

    minor changes to initial html

--- src/main.ts
+++ src/main.ts
@@ -10,7 +10,8 @@ document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
 <main>
   <h1>Ron Reade</h1>
   <p><label for="ron_text">Enter text:</label></p>
-  <textarea name="ron_text" id="ron_text" rows="20" cols="33">Know thyself</textarea>
+  <textarea name="ron_text" id="ron_text" rows="20" cols="33"
+    placeholder="Enter text here"></textarea>
   <button id="process_text_button" type="button" disabled>Read Aloud</button>
   <button id="clear_button" type="button" disabled>Clear Text</button>
   <button id="halt_button" type="button" disabled>Halt Processing</button>
